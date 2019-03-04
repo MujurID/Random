0a6605c59ae00570124fee42e5627ae9684b2621
MujurID <40834757+MujurID@users.noreply.github.com>
2019-03-04 14:36:39 +0700

    Update random.php

--- random.php
+++ random.php
@@ -1,6 +1,6 @@
 <?php
 $panjang= '10'; //panjang karakter yang akan dibuat
-$data = "0123456789abcdefghijklmnopqrstuvwxyz"; //data yang akan diacak
+$data = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; //data yang akan diacak
 $hasil = ''; //jangan lupa ini kosong eaa
 for($l = 0; $l<$panjang; $l++){
     $hasil .= substr(str_shuffle($data),-1);
