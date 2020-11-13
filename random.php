3ddd306a801ffe0f2f5bf44aa5049a27020b98e2
MujurID <mujurid@students.undip.ac.id>
2020-11-13 14:39:50 +0700

    Update random.php

--- random.php
+++ random.php
@@ -1,8 +1,26 @@
 <?php
+//=========RANDOM ACAK==========
 $panjang= '10'; //panjang karakter yang akan dibuat
 $data = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; //data yang akan diacak
-$hasil = ''; //jangan lupa ini kosong eaa
+$hasil0 = ''; //jangan lupa ini kosong eaa
 for($l = 0; $l<$panjang; $l++){
-    $hasil .= substr(str_shuffle($data),-1);
+    $hasil0 .= substr(str_shuffle($data),-1);
 }
-echo $hasil; //hasil akhirnya
+$hasil1 = $hasi0;
+echo $hasil1; //hasil akhirnya
+
+
+
+
+//=======RANDOM DENGAN JUMLAH DIGITS + 0
+$min = 1;
+$max = 200;
+$digits = strlen($max);
+$hasil2 = str_pad(rand($min, $max), $digits, '0', STR_PAD_LEFT);
+echo $hasil2;
+
+
+
+
+
+//gw noob
