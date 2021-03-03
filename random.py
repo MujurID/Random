20bf896885106be5c5b47140f49b54b0e282a82b
MujurID <mujurid@students.undip.ac.id>
2021-03-03 17:05:10 +0700

    Update random.py

--- random.py
+++ random.py
@@ -1,7 +1,7 @@
 import random
 
 # Random string of length 5
-panjang = 
+panjang = 5
 result_str = ''.join((random.choice('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(panjang)))
 print(result_str)
 hasil = result_str
